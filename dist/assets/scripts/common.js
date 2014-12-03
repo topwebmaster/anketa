09d2d5572d6e1e45a8fd923cca52e92757fd0eef
Topwebmaster <topwebmasterblog@gmail.com>
2014-12-03 12:40:56 +0300

    begin range slider

--- dist/assets/scripts/common.js
+++ dist/assets/scripts/common.js
@@ -1,4 +1,8 @@
 $(function () {
 	'use strict';
+	// first we need a slider to work with
+	var $slider2 = $("#slider2").slider({ max: 20 , value: 10 });
 
+// and then we can apply pips to it!
+	$slider2.slider("pips");
 });
