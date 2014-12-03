fb8f86da568b50918c01ee81530a8b44d248b2ae
Topwebmaster <topwebmasterblog@gmail.com>
2014-12-03 17:40:18 +0300

    init

--- dist/assets/scripts/common.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-	'use strict';
-	// first we need a slider to work with
-	var $slider2 = $("#slider2").slider({ max: 20 , value: 10 });
-
-// and then we can apply pips to it!
-	$slider2.slider("pips");
-});
